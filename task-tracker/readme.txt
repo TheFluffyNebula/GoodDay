f512165fa84c3999806f17798827dec0e23e6840
Jason Yin <jyin23@ncsu.edu>
2023-12-01 15:56:01 -0500

    UI from tutorial complete!

--- task-tracker/readme.txt
+++ task-tracker/readme.txt
@@ -3,10 +3,14 @@ I'm using this tutorial: https://www.youtube.com/watch?v=w7ejDZ8SWv8
 stuff to download: node.js
 terminal: npm i react-icons
 to run it: cd into task-tracker folder, then input "npm start" into terminal
-probably a good idea: react chrome extension, VERY nice for debugging (litearl popup instead of console pain)
 
+React chrome extension, VERY nice for debugging (ex. seeing values of components)
+Not showing up? Use this fix:
+"HEY all, found a way to fix this issue..click inspect, go into settings , click ignore list, 
+under General exclusion rules: uncheck "Content scripts injected by extensions".. 
 
 vsc stuff:
 get the ES7+ React extension (some shortcuts)
-change the language bottom right from JS to JSX
+rafce: new component, clg: console.log
+optional: change the language bottom right from JS to JSX
 misc: vs code pets! (get the extension, then click the squirrel icon bot right)
--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 
 function App() { // /* */ in braces for comments
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1, 
@@ -23,12 +26,41 @@ function App() { // /* */ in braces for comments
         reminder: false, 
     }
 ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+  }
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => 
+    task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+      task.id === id ? { ...task, reminder: 
+        !task.reminder } : task
+      )
+    )
+  }
+
   return (
     <div className='container'> {/* for fragment, empty angle brackets here*/}
-      <Header />
-      <Tasks tasks={tasks} />
+      <Header 
+      onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />} 
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete=
+      {deleteTask} onToggle={toggleReminder} /> 
+      ) : ('No Tasks To Show')}
     </div>
-  );
+  )
 }
 
 /* class based component
